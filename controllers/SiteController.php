fb16c633501cb51add8fba4dd99f4731a527b413
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-07-31 08:57:30 -0300

    Add controllers

---
+++ controllers/SiteController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace app\controllers;
+
+use app\forms\LoginForm;
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\rest\Controller;
+use yii\web\Response;
+
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions(): array
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex(): string
+    {
+        return $this->render('index');
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin(): Response|string
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        $loginData = Yii::$app->request->post();
+
+        if ($model->load($loginData) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout(): Response
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+}
---
+++ controllers/WorkController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\controllers;
+
+use app\entities\Work;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+use yii\web\Response;
+
+class WorkController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors(): array
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'index' => ['get'],
+                    'create' => ['get', 'post'],
+                    'update' => ['get', 'post'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex(): string
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Work::find(),
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionCreate(): string|Response
+    {
+        $model = new Work();
+
+        if ($this->request->isPost && $this->saveModel($model)) {
+            return $this->redirect(['index']);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionUpdate(int $id): string|Response
+    {
+        $model = Work::findOne($id);
+
+        if ($this->request->isPost && $this->saveModel($model)) {
+            return $this->redirect(['index']);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    private function saveModel(Work $model): bool
+    {
+        if (!$model->load($this->request->post())) {
+            return false;
+        }
+
+        $transaction = Yii::$app->db->beginTransaction();
+
+        try {
+            $model->saveOrFail();
+            $transaction->commit();
+            Yii::$app->session->setFlash('success', 'Obra salva com sucesso!');
+        } catch (\Exception $e) {
+            Yii::$app->session->setFlash('error', 'Não foi possível salvar a obra.');
+            return false;
+        } finally {
+            $transaction->isActive && $transaction->rollBack();
+        }
+
+        return true;
+    }
+}
