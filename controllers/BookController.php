82a093fd0ac456f8a5c3587c49fa0845b2c5f0fe
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-07-31 09:55:36 -0300

    Create BookController

---
+++ controllers/BookController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\exceptions\FriendlyException;
+use app\entities\Book;
+use app\forms\BookForm;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+use yii\web\Response;
+
+class BookController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors(): array
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'index' => ['get'],
+                    'create' => ['get', 'post'],
+                    'update' => ['get', 'post'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex(): string
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Book::find(),
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionCreate(): string|Response
+    {
+        $model = new BookForm();
+
+        if ($this->request->isPost && $this->saveModel($model)) {
+            return $this->redirect(['index']);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionUpdate(int $id): string|Response
+    {
+        $model = BookForm::findOne($id);
+
+        if ($this->request->isPost && $this->saveModel($model)) {
+            return $this->redirect(['index']);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    private function saveModel(BookForm $model): bool
+    {
+        if (!$model->load($this->request->post())) {
+            return false;
+        }
+
+        $transaction = Yii::$app->db->beginTransaction();
+
+        try {
+            $model->saveOrFail();
+            $transaction->commit();
+            Yii::$app->session->setFlash('success', 'Livro salvo com sucesso!');
+
+            return true;
+        } catch (FriendlyException $friendlyException) {
+            Yii::$app->session->setFlash('error', $friendlyException->getMessage());
+        } catch (\Exception $e) {
+            Yii::$app->session->setFlash('error', 'Não foi possível salvar o livro.');
+        } finally {
+            $transaction->isActive && $transaction->rollBack();
+        }
+
+        return false;
+    }
+}
