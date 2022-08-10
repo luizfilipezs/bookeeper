717adccc72f89debbd3e2cd44486f684ecdacce4
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-08-10 00:18:24 -0300

    Remove desnecessary documentation lines

--- core/db/subscription/AfterInsert.php
+++ core/db/subscription/AfterInsert.php
@@ -7,19 +7,6 @@ use Attribute;
 /**
  * Subscriber method attribute that marks the method to be called on event
  * `app\core\db\ActiveRecord::EVENT_AFTER_INSERT`.
- * 
- * Usage:
- * 
- * ```php
- * class UserSubscriber
- * {
- *     #[AfterInsert]
- *     public function sendWelcomeMessage(User $user): void
- *     {
- *         // ...
- *     }
- * }
- * ```
  */
 #[Attribute(Attribute::TARGET_METHOD)]
 class AfterInsert {
