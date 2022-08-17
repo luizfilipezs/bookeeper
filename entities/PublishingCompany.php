dfe51d8ded68ad53a7895c9e9af60596f0dc9471
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-08-17 15:33:49 -0300

    Improvements on PublishingCompany

--- entities/PublishingCompany.php
+++ entities/PublishingCompany.php
@@ -24,7 +24,7 @@ class PublishingCompany extends ActiveRecord
      */
     public static function findByName(string $name): ?static
     {
-        return self::findOne(['name' => $name]);
+        return static::findOne(['name' => $name]);
     }
 
     /**
@@ -33,8 +33,8 @@ class PublishingCompany extends ActiveRecord
     public function rules(): array
     {
         return [
-            [['name'], 'required'],
-            [['name'], 'string'],
+            ['name', 'required'],
+            ['name', 'string'],
         ];
     }
 
