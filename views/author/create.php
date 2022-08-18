f10a599fa996952e57c611f2ea47db19c60a38dc
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-08-18 14:56:08 -0300

    Create author views

---
+++ views/author/create.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @var yii\web\View $this
+ * @var app\forms\WorkForm $model
+ */
+
+$this->title = 'Cadastrar autor';
+
+echo $this->render('_form', [
+    'model' => $model,
+]);
---
+++ views/author/update.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @var yii\web\View $this
+ * @var app\forms\WorkForm $model
+ */
+
+$this->title = 'Editar autor';
+
+echo $this->render('_form', [
+    'model' => $model,
+]);
