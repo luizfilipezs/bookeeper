8fec30f3245aee839ad2dad21095f903869353d7
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-08-23 09:35:08 -0300

    Add docs to param

--- core/web/IActionView.php
+++ core/web/IActionView.php
@@ -10,6 +10,8 @@ interface IActionView
     /**
      * Renders a detail view.
      * 
+     * @param int $id Record ID.
+     * 
      * @return string Rendered view.
      */
     public function actionView(int $id): string;
