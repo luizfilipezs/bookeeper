8477433b40dc6a3ecfe539044b638e75d68ee0d6
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-08-25 20:18:02 -0300

    Add login/logout item on menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -43,20 +43,18 @@ AppAsset::register($this);
             ['label' => 'Obras', 'url' => ['/work']],
             ['label' => 'Autores', 'url' => ['/author']],
             ['label' => 'Tags', 'url' => ['/tag']],
-            /*
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                ['label' => 'Entrar', 'url' => ['/site/login']]
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
                 . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    'Sair (' . Yii::$app->user->identity->username . ')',
                     ['class' => 'btn btn-link logout']
                 )
                 . Html::endForm()
                 . '</li>'
             )
-            */
         ],
     ]);
     NavBar::end();
