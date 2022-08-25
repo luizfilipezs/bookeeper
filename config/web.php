d5e0a94f9f8b11adfe3c2116ac15f23ccea1e338
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-08-25 20:16:25 -0300

    Install DateControl widget

--- config/web.php
+++ config/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use kartik\datecontrol\Module;
+
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 $cookieValidationKey = require __DIR__ . '/cookieValidationKey.php';
@@ -60,6 +62,57 @@ $config = [
         ],
     ],
     'params' => $params,
+    'modules' => [
+        'datecontrol' =>  [
+            'class' => 'kartik\datecontrol\Module',
+            'displaySettings' => [
+                Module::FORMAT_DATE => 'dd-MM-yyyy',
+                Module::FORMAT_TIME => 'HH:mm:ss a',
+                Module::FORMAT_DATETIME => 'dd-MM-yyyy HH:mm:ss a',
+            ],
+
+            'saveSettings' => [
+                Module::FORMAT_DATE => 'php:Y-m-d',
+                Module::FORMAT_TIME => 'php:H:i:s',
+                Module::FORMAT_DATETIME => 'php:Y-m-d H:i:s',
+            ],
+
+            'displayTimezone' => 'America/Sao_Paulo',
+            'saveTimezone' => 'America/Sao_Paulo',
+
+            // automatically use kartik\widgets for each of the above formats
+            'autoWidget' => true,
+
+            // use ajax conversion for processing dates from display format to save format.
+            'ajaxConversion' => true,
+
+            // default settings for each widget from kartik\widgets used when autoWidget is true
+            'autoWidgetSettings' => [
+                Module::FORMAT_DATE => [
+                    'type' => 2,
+                    'pluginOptions' => [
+                        'autoclose' => true
+                    ]
+                ],
+                Module::FORMAT_DATETIME => [],
+                Module::FORMAT_TIME => [],
+            ],
+
+            // custom widget settings that will be used to render the date input instead of kartik\widgets,
+            // this will be used when autoWidget is set to false at module or widget level.
+            /*
+            'widgetSettings' => [
+                Module::FORMAT_DATE => [
+                    'class' => 'yii\jui\DatePicker', // example
+                    'options' => [
+                        'dateFormat' => 'php:d-M-Y',
+                        'options' => ['class' => 'form-control'],
+                    ]
+                ]
+            ],
+            */
+        ]
+    ],
 ];
 
 if (YII_ENV_DEV) {
