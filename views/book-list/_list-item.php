ba150381233cc8a22d0d9d618235e2f07751e022
Luiz Filipe da Silva <filipeluiz.bs@gmail.com>
2022-09-05 16:33:55 -0300

    Update book list item

--- views/book-list/_list-item.php
+++ views/book-list/_list-item.php
@@ -10,14 +10,8 @@ use yii\helpers\Html;
 ?>
 
 <div class="d-flex flex-row justify-content-start" book-id="<?= $model->id ?>">
-    <div>
-        <?= Html::img('https://m.media-amazon.com/images/I/41rw7ra7UmL.jpg', [
-            'style' => 'width: 100px',
-        ]) ?>
-    </div>
     <div class="flex-grow-1 p-4">
-        <p><?= $model->title ?></p>
-        <p><?= $model->subtitle ?></p>
+        <p><?= $model->title . ($model->subtitle ? ' — ' . $model->subtitle : '') ?></p>
         <p class="text-secondary">
             <i><?= implode(', ', $model->authorNames) ?></i>
         </p>
